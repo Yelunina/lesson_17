f95c168371d2adb9929863acf235c235f70d20e7
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-08-09 21:16:32 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,6 @@ import java.util.Scanner;
 // который возвращает строку с данными студента.
 public class Main {
     public static void main(String[] args) {
-        /*
-            #1 Создать класс Student со следующими полями (String firstName, String lastName,
-            MyDate birthday, String group, MyDate startCourse}
-            Создайте пару объектов этого класса. Напишите метод studentToString(Student student),
-            который возвращает строку с данными студента.
-
-         */
-
 
         System.out.println("Hello! You're welcome on my page\n");
         Scanner scanner = new Scanner(System.in);
